d29104ae8dd593610443008147a9d47a1d4e1093
Karthik Malugu <134277800+kmalugu@users.noreply.github.com>
2026-03-26 11:19:17 +0530

    Final commit

--- grp10_pro/utils/embeddings.py
+++ grp10_pro/utils/embeddings.py
@@ -1,11 +1,9 @@
 # Embeddings Utility
 
-from functools import lru_cache
-
 from langchain_huggingface import HuggingFaceEmbeddings
 
 from utils.config import EMBEDDING_MODEL
-
+from functools import lru_cache
 
 @lru_cache(maxsize=1)
 def get_embeddings():
